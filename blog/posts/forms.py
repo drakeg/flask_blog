32883b914d92eadd6be27b8de83958d81a81de45
Greg Drake <greg@madmallards.com>
2024-02-23 17:57:53 -0500

    Updated requirements.txt and cleaned up.

--- blog/posts/forms.py
+++ blog/posts/forms.py
@@ -2,7 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
 
-
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
