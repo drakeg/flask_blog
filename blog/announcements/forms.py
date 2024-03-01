60e43afcff4b44ba92196cddbdf7268423ac1f14
Greg Drake <greg@madmallards.com>
2024-03-01 00:47:28 -0500

    Updated new announcement pages with CKEditor.

--- blog/announcements/forms.py
+++ blog/announcements/forms.py
@@ -3,10 +3,11 @@ from wtforms import StringField, TextAreaField, SubmitField, DateField
 from wtforms.validators import DataRequired
 from datetime import date
 from flask_pagedown.fields import PageDownField
+from flask_ckeditor import CKEditorField
 
 class AnnouncementForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
-    content = PageDownField('Content', validators=[DataRequired()])
+    content = CKEditorField('Content', validators=[DataRequired()])
     start_date = DateField('Start Date', validators=[DataRequired()], default=date.today, format='%Y-%m-%d')
     end_date = DateField('End Date', validators=[DataRequired()], default=date.today, format='%Y-%m-%d')
     submit = SubmitField('Post Announcement')
