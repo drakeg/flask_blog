5373bba0432bcf0b5b7073499d5a5e4645e54967
Greg Drake <greg@madmallards.com>
2024-03-01 00:43:51 -0500

    Added CKEditor fields

--- blog/posts/forms.py
+++ blog/posts/forms.py
@@ -2,8 +2,9 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
 from flask_pagedown.fields import PageDownField
+from flask_ckeditor import CKEditorField
 
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
-    content = PageDownField('Content', validators=[DataRequired()])
+    content = CKEditorField('Content', validators=[DataRequired()])
     submit = SubmitField('Post')
